9dd86bcab9a25376a7b14490e0a4dd7753040aa4
JULINHOGT23 <1485840@senati.pe>
2023-04-23 14:54:56 -0500

    asemos que se oculte el menu una vez escogido una opcion

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ function mostrarOcultarMenu(){
         menu_visible = false;
     }
 }
+//oculto el menu una vez que selecciono una opción
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
