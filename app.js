a820778a28f38e684ad1c38b7185890dd227a15d
JULINHOGT23 <1485840@senati.pe>
2023-04-23 14:59:43 -0500

    selecciono todas las barras generales par aluego manipularlas

--- app.js
+++ app.js
@@ -19,3 +19,24 @@ for(var x = 0; x <links.length;x++){
         menu_visible = false;
     }
 }
+//Creo las barritas de una barra particular identificada por su id
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+    }
+}
+//selecciono todas las barras generales par aluego manipularlas
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let wordpress = document.getElementById("wordpress");
+crearBarra(wordpress);
+let photoshop = document.getElementById("photoshop");
+crearBarra(photoshop);
+let php = document.getElementById("php");
+crearBarra(php);
+let ilustrator = document.getElementById("ilustrator");
+crearBarra(ilustrator);
