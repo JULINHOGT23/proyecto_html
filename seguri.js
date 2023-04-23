71d9401516d7a902ac13970ebb4b6a3afdb890b7
JULINHOGT23 <1485840@senati.pe>
2023-04-23 08:46:44 -0500

    agregamos un archibo js y los links de mis redes sociales

---
+++ seguri.js
@@ -0,0 +1,5 @@
+const logrebox = document.querySelector('. logreg-box');
+const loginlink = document.querySelector('. login-link');
+const registerlink = document.querySelector('. register-link');
+
+registerlink.addEventListener('click', () => {logrebox.classList.add('active')});
